3e9a3979104c3ae29682a4a1cfad0b009ca5fbdd
Edward P. Legaspi <czetsuya@gmail.com>
2019-06-24 06:48:16 +0800

    Add loader.

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -4,6 +4,7 @@ import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import ApplicationStore from './framework/redux/ApplicationStore';
 import App from './App';
+import Loader from 'react-loader-spinner'
 
 class AppWrapper extends Component {
 	constructor(props) {
@@ -14,7 +15,7 @@ class AppWrapper extends Component {
 	render() {
 		return (
 			<Provider store={ApplicationStore}>
-				<PersistGate loading={null} persistor={this.persistor}>
+				<PersistGate loading={<Loader type="Triangle" />} persistor={this.persistor}>
 					<App />
 				</PersistGate>
 			</Provider>
